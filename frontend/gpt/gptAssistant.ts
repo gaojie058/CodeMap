ccfefa5dabc3cfcb5395e0219ebd51e59ac14938
somin0016 <tsoominn@gmail.com>
2024-09-06 16:57:24 +0800

    update gptassistant id

--- frontend/gpt/gptAssistant.ts
+++ frontend/gpt/gptAssistant.ts
@@ -55,7 +55,7 @@ async function updateAssistant(assistantId: string) {
 }
 
 export async function useAssistant(promptName: PromptName, content: string): Promise<string> {
-    const assistantId = "asst_dTXAcHRrR3oIQqrUIkzrgdvw";//task1
+    const assistantId = "asst_1rm5Iq7odQeJcMwC8sCbAzx3";//task1
     // const assistantId= "asst_cj2CTaMuDQxc7l4YA3UtZ3nX"//task2
     console.log(`Using assistant ${assistantId}...`);
     
