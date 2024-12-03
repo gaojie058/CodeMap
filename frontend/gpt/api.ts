d1ad0db21d4a0cb05d40194254eb9a0a9fff8c83
gaojie058 <gaojie058@163.com>
2024-12-04 02:14:40 +0800

    add django assistant

--- frontend/gpt/api.ts
+++ frontend/gpt/api.ts
@@ -1,8 +1,10 @@
 export const API_KEY = import.meta.env.VITE_OPENAI_API_KEY || '';
 
 // Load assistantid environment variables from Vite (via import.meta.env)
-const ASSISTANT_FASTAPI = import.meta.env.VITE_ASSISTANT_FASTAPI || 'assistant1';
-const ASSISTANT_FLASK = import.meta.env.VITE_ASSISTANT_FLASK || 'assistant2';
+const ASSISTANT_FASTAPI = import.meta.env.VITE_ASSISTANT_FASTAPI || '';
+const ASSISTANT_FLASK = import.meta.env.VITE_ASSISTANT_FLASK || '';
+const ASSISTANT_DJANGO = import.meta.env.VITE_ASSISTANT_DJANGO || '';
+
 const CURRENT_ASSISTANT = import.meta.env.VITE_CURRENT_ASSISTANT || '';
 
 
@@ -14,8 +16,11 @@ export const assistantId = (() => {
     } else if (CURRENT_ASSISTANT.toLowerCase() === 'flask') {
         console.log("Setting assistantId: Current assistant will be flask")
         return ASSISTANT_FLASK;
+    } else if (CURRENT_ASSISTANT.toLowerCase() === 'django') {
+        console.log("Setting assistantId: Current assistant will be django")
+        return ASSISTANT_DJANGO;
     } else {
-        throw new Error("Invalid value for assistantId & CURRENT_ASSISTANT. Must be 'fastapi' or 'flask'.");
+        throw new Error("Invalid value for assistantId & CURRENT_ASSISTANT. Must be 'fastapi' or 'flask' or 'django'");
     }
 })();
 
@@ -25,6 +30,7 @@ console.log(`The current active assistant ID is: ${assistantId}`);
 // Load filepath environment variables from Vite (via import.meta.env)
 const FILE_PATH_FASTAPI = import.meta.env.VITE_FILE_PATH_FASTAPI
 const FILE_PATH_FLASK = import.meta.env.VITE_FILE_PATH_FLASK
+const FILE_PATH_DJANGO = import.meta.env.VITE_FILE_PATH_DJANGO
 
 
 // Determine the active assistant
@@ -35,8 +41,11 @@ export const filepath = (() => {
     } else if (CURRENT_ASSISTANT.toLowerCase() === 'flask') {
         console.log("Setting filepath: Current path will be flask")
         return FILE_PATH_FLASK;
+    } else if (CURRENT_ASSISTANT.toLowerCase() === 'django') {
+        console.log("Setting assistantId: Current path will be django")
+        return FILE_PATH_DJANGO;
     } else {
-        throw new Error("Invalid value for filepath & CURRENT_ASSISTANT. Must be 'fastapi' or 'flask'.");
+        throw new Error("Invalid value for filepath & CURRENT_ASSISTANT. Must be 'fastapi' or 'flask' or 'django'");
     }
 })();
 
